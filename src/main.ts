99afa36788cf77bd9b119aa8a4c5463db2928e40
Az-Yql <3254827649@qq.com>
2024-05-14 19:29:29 +0800

    封装axios加配置环境

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+import { setRegisterGlobal } from "./global";
+
+const app = createApp(App);
+app.use(setRegisterGlobal);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
